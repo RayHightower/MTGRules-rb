76a02bf1a0779e6252aeb2af05e59abed97b4af5
Dave Astels <dastels@me.com>
2012-08-08 23:35:47 -0500

    Get search controller working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,14 @@
 class SearchController < UIViewController
   extend IB
 
-  attr_accessor :delegate, :searchBar, :resultsView, :searchDisplayController
+  attr_accessor :delegate, :searchBar, :resultsView, :searchDisplayController, :tableCell
 
 
   ## ib outlets
   ib_outlet :searchBar, UISearchBar
   ib_outlet :resultsView, UITableView
   ib_outlet :searchDisplayController, UISearchDisplayController
+  ib_outlet :tableCell, UITableViewCell
 
   CELL_IDENTIFIER = "RuleCell"
 
@@ -31,8 +32,8 @@ class SearchController < UIViewController
 
 
   def show_references_for(fragment)
-    results = delegate.search_for(fragment)
-    if results.size > 0
+    @results = delegate.search_for(fragment)
+    if @results.size > 0
       searchDisplayController.searchResultsTableView.reloadData
       searchDisplayController.searchResultsTableView.scrollToRowAtIndexPath(NSIndexPath.indexPathForRow(0, inSection: 0), atScrollPosition: UITableViewScrollPositionTop, animated: false)
     end
@@ -51,54 +52,55 @@ class SearchController < UIViewController
 
 
   def tableView(table_view, numberOfRowsInSection:section)
-    results.count
+    @results ||= []
+    @results.count
   end
 
 
   def body_height_for(text)
     cell_font = UIFont.fontWithName("Helvetica", size: 14.0)
     constraint_size = CGSizeMake(self.view.frame.size.width - 40, 100000.0)
-    labelSize = text.sizeWithFont(cellFont, constrainedToSize: constraint_size, lineBreakMode: UILineBreakModeWordWrap)
+    label_size = text.sizeWithFont(cell_font, constrainedToSize: constraint_size, lineBreakMode: UILineBreakModeWordWrap)
     label_size.height
   end
 
 
   def get_cell_text_at_index_path(index_path)
-    results[indexPath.row].body
+    @results[index_path.row].body
   end
 
 
   def tableView(table_view, heightForRowAtIndexPath: index_path)
-    body_height_for(get_cell_text_at_index_path(indexPath)) + 54
+    body_height_for(get_cell_text_at_index_path(index_path)) + 54
   end
 
 
   def tableView(table_view, cellForRowAtIndexPath: index_path)
-    cell = table_view.dequeueReusableCellWithIdentifier(CellIdentifier)
+    cell = table_view.dequeueReusableCellWithIdentifier(CELL_IDENTIFIER)
     if cell.nil?
-      NSBundle.mainBundle.loadNibNamed(CellIdentifier, owner: self, options: nil)
+      NSBundle.mainBundle.loadNibNamed(CELL_IDENTIFIER, owner: self, options: nil)
       cell = tableCell
       tableCell = nil
     end
   
     header_label = cell.viewWithTag(1)
-    bodyLabel = cell.viewWithTag(2)
+    body_label = cell.viewWithTag(2)
     
-    detail = results[indexPath.row]
+    detail = @results[index_path.row]
     header_label.text = detail.name
     body_label.text = detail.body
     body_label.lineBreakMode = UILineBreakModeWordWrap
     body_label.numberOfLines = 0
     body_label.font = UIFont.fontWithName("Helvetica", size: 14.0)
-    body_rame = body_label.frame
+    body_frame = body_label.frame
     body_frame.size.height = body_height_for(body_label.text)
     body_label.frame = body_frame
     cell
   end
 
 
-  def tableView(table_view, didSelectRowAtIndexPath: indexPath)
-    detail = results[indexPath.row]
+  def tableView(table_view, didSelectRowAtIndexPath: index_path)
+    detail = @results[index_path.row]
     referenced_rules = if detail.instance_of?(GlossaryEntry)
                          delegate.get_rules_referenced_by_glossary_term(detail.term)
                        else
